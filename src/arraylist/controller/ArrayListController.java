38370d9917a6df0e692eb5c4a7fbe775df2a94a6
meanmonkey662 <kkoc6943@740s-pl-132.local>
2015-10-07 13:54:35 -0600

    Added array list controller

--- src/arraylist/controller/ArrayListController.java
+++ src/arraylist/controller/ArrayListController.java
@@ -1,12 +1,31 @@
 package arraylist.controller;
 
 import java.util.ArrayList;
+import lists.view.ListViewer;
 
 public class ArrayListController
 {
 	private ArrayList<String> firstWords;
+	private ListViewer popupDisplay;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+		popupDisplay = new ListViewer();
+	}
+	
 	public void start()
 	{
-		firstWords.add(";alskdfjl;awjgfiojaopbhoaejnmvp[iosdhtnpatuj[i2	r[oq;ijfpahwl;;oiqwrgpeisugnk.ashtbp3	wtgoabewy");
+		firstWords.add("; asl;dkfj;lajgqgj[o0q35hyqk;ebnvilqw;enbviqop");
+		firstWords.add(" babababababaababbaabbabababbaba");
+		displayList();
+	}
+	
+	private void displayList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			popupDisplay.showList("The contents at " + spot + "are" + firstWords.get(spot));
+		}
 	}
 }
